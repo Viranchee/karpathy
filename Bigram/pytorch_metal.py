5a989701ec5db896e9f93c023b5f1dd505d2c300
Viranchee Lotia <hey@viranchee.com>
2024-09-21 23:35:24 -0400

    Unfinished work

--- Bigram/pytorch_metal.py
+++ Bigram/pytorch_metal.py
@@ -14,86 +14,217 @@ from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
 
 
-class Charset(Dataset):
-  def __init__(self, names: List[str], maxLen: int):
-    self.names = names
-    self.maxLen = maxLen
-    self.chars = sorted(list(set("".join(names))))
-    self.c2i = {ch: i for i, ch in enumerate(self.chars)}
-    self.i2c = {i: ch for i, ch in enumerate(self.chars)}
-
-  def __len__(self):
-    return len(self.names)
-
-  def contains(self, name):
-    return name in self.names
-
-  def get_vocab_size(self):
-    return len(self.chars)
-
-  def encode(self, name):
-    ix = torch.tensor([self.c2i[ch] for ch in name])
-    return ix
-
-  def decode(self, ix):
-    return "".join(self.i2c[i] for i in ix)
-
-  def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor]:
-    word = self.names[index]
-    ix = self.encode(word)
-    x = torch.zeros(self.maxLen + 1, dtype=torch.long)
-    y = torch.zeros(self.maxLen + 1, dtype=torch.long)
-    x[1 : 1 + len(ix)] = ix
-    y[len(ix) + 1 :] = -1
-    return x, y
-
-
-def create_datasets(file: str) -> tuple[Charset, Charset]:
-  with open(file) as f:
-    names = f.read().splitlines()
-  names = [name.lower().strip() for name in names]
-  names = [name for name in names if name]
-  # Use functional python style, "".join(names) -> set -> list
-  chars = sorted(list(set("".join(names))))
-  maxLen = max(len(name) for name in names)
-  testSetSize = len(names) // 10
-  rp = torch.randperm(len(names))
-  trainNames = [names[i] for i in rp[testSetSize:]]
-  testNames = [names[i] for i in rp[:testSetSize]]
-  trainSet = Charset(trainNames, maxLen)
-  testSet = Charset(testNames, maxLen)
-  return trainSet, testSet
-
-
 ### PyTorch implementation
 class Bigram(nn.Module):
-  def __init__(self, vocab_size: int):
-    super().__init__()
-    n = vocab_size
-    self.logits = nn.Parameter(torch.randn((n, n)))
+    def __init__(self, vocab_size: int):
+        super().__init__()
+        n = vocab_size
+        self.logits = nn.Parameter(torch.randn((n, n)))  # 2D square matrix
 
-  def forward(self, idx, targets=None):
-    logits = self.logits[idx]
-    loss = None
-    if targets is not None:
-      loss = F.cross_entropy(
-        logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
-      )
-    return logits, loss
+    def forward(self, idx, targets=None):
+        logits = self.logits[idx]  # Extract rows
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)),  # 2D, figure out the shape
+                targets.view(-1),
+                ignore_index=-1,
+            )
+        return logits, loss
 
 
-def pytorch(args, testData, trainData):
-  model = Bigram(vocab_size=27)
+@torch.no_grad()
+def generate(model, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None):
+    """
+    Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+    the sequence max_new_tokens times, feeding the predictions back into the model each time.
+    Most likely you'll want to make sure to be in model.eval() mode of operation for this.
+    """
+    block_size = model.get_block_size()
+    for _ in range(max_new_tokens):
+        # if the sequence context is growing too long we must crop it at block_size
+        idx_cond = idx if idx.size(1) <= block_size else idx[:, -block_size:]
+        # forward the model to get the logits for the index in the sequence
+        logits, _ = model(idx_cond)
+        # pluck the logits at the final step and scale by desired temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop the logits to only the top k options
+        if top_k is not None:
+            v, _ = torch.topk(logits, top_k)
+            logits[logits < v[:, [-1]]] = -float("Inf")
+        # apply softmax to convert logits to (normalized) probabilities
+        probs = F.softmax(logits, dim=-1)
+        # either sample from the distribution or take the most likely element
+        if do_sample:
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            _, idx_next = torch.topk(probs, k=1, dim=-1)
+        # append sampled index to the running sequence and continue
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
+
+
+def print_samples(num=10):
+    """samples from the model and pretty prints the decoded samples"""
+    X_init = torch.zeros(num, 1, dtype=torch.long).to(args.device)
+    top_k = args.top_k if args.top_k != -1 else None
+    steps = (
+        train_dataset.get_output_length() - 1
+    )  # -1 because we already start with <START> token (index 0)
+    X_samp = generate(model, X_init, steps, top_k=top_k, do_sample=True).to("cpu")
+    train_samples, test_samples, new_samples = [], [], []
+    for i in range(X_samp.size(0)):
+        # get the i'th row of sampled integers, as python list
+        row = X_samp[
+            i, 1:
+        ].tolist()  # note: we need to crop out the first <START> token
+        # token 0 is the <STOP> token, so we crop the output sequence at that point
+        crop_index = row.index(0) if 0 in row else len(row)
+        row = row[:crop_index]
+        word_samp = train_dataset.decode(row)
+        # separately track samples that we have and have not seen before
+        if train_dataset.contains(word_samp):
+            train_samples.append(word_samp)
+        elif test_dataset.contains(word_samp):
+            test_samples.append(word_samp)
+        else:
+            new_samples.append(word_samp)
+    print("-" * 80)
+    for lst, desc in [
+        (train_samples, "in train"),
+        (test_samples, "in test"),
+        (new_samples, "new"),
+    ]:
+        print(f"{len(lst)} samples that are {desc}:")
+        for word in lst:
+            print(word)
+    print("-" * 80)
+
+
+@torch.inference_mode()
+def evaluate(model, dataset, batch_size=50, max_batches=None):
+    model.eval()
+    loader = DataLoader(dataset, shuffle=True, batch_size=batch_size, num_workers=0)
+    losses = []
+    for i, batch in enumerate(loader):
+        batch = [t.to(args.device) for t in batch]
+        X, Y = batch
+        logits, loss = model(X, Y)
+        losses.append(loss.item())
+        if max_batches is not None and i >= max_batches:
+            break
+    mean_loss = torch.tensor(losses).mean().item()
+    model.train()  # reset model back to training mode
+    return mean_loss
+
+
+# -----------------------------------------------------------------------------
+# helper functions for creating the training and test Datasets that emit words
+
+
+class CharDataset(Dataset):
+    def __init__(self, words, chars, max_word_length):
+        self.words = words
+        self.chars = chars
+        self.max_word_length = max_word_length
+        self.stoi = {ch: i + 1 for i, ch in enumerate(chars)}
+        self.itos = {i: s for s, i in self.stoi.items()}  # inverse mapping
+
+    def __len__(self):
+        return len(self.words)
+
+    def contains(self, word):
+        return word in self.words
+
+    def get_vocab_size(self):
+        return len(self.chars) + 1  # all the possible characters and special 0 token
+
+    def get_output_length(self):
+        return self.max_word_length + 1  # <START> token followed by words
+
+    def encode(self, word):
+        ix = torch.tensor([self.stoi[w] for w in word], dtype=torch.long)
+        return ix
+
+    def decode(self, ix):
+        word = "".join(self.itos[i] for i in ix)
+        return word
+
+    def __getitem__(self, idx):
+        word = self.words[idx]
+        ix = self.encode(word)
+        x = torch.zeros(self.max_word_length + 1, dtype=torch.long)
+        y = torch.zeros(self.max_word_length + 1, dtype=torch.long)
+        x[1 : 1 + len(ix)] = ix
+        y[: len(ix)] = ix
+        y[len(ix) + 1 :] = -1  # index -1 will mask the loss at the inactive locations
+        return x, y
+
+
+def create_datasets(input_file):
+    # preprocessing of the input text file
+    with open(input_file, "r") as f:
+        data = f.read()
+    words = data.splitlines()
+    words = [w.strip() for w in words]  # get rid of any leading or trailing white space
+    words = [w for w in words if w]  # get rid of any empty strings
+    chars = sorted(list(set("".join(words))))  # all the possible characters
+    max_word_length = max(len(w) for w in words)
+    print(f"number of examples in the dataset: {len(words)}")
+    print(f"max word length: {max_word_length}")
+    print(f"number of unique characters in the vocabulary: {len(chars)}")
+    print("vocabulary:")
+    print("".join(chars))
+
+    # partition the input data into a training and the test set
+    test_set_size = min(
+        1000, int(len(words) * 0.1)
+    )  # 10% of the training set, or up to 1000 examples
+    rp = torch.randperm(len(words)).tolist()
+    train_words = [words[i] for i in rp[:-test_set_size]]
+    test_words = [words[i] for i in rp[-test_set_size:]]
+    print(
+        f"split up the dataset into {len(train_words)} training examples and {len(test_words)} test examples"
+    )
+
+    # wrap in dataset objects
+    train_dataset = CharDataset(train_words, chars, max_word_length)
+    test_dataset = CharDataset(test_words, chars, max_word_length)
+
+    return train_dataset, test_dataset
+
+
+class InfiniteDataLoader:
+    """
+    this is really hacky and I'm not proud of it, but there doesn't seem to be
+    a better way in PyTorch to just create an infinite dataloader?
+    """
+
+    def __init__(self, dataset, **kwargs):
+        train_sampler = torch.utils.data.RandomSampler(
+            dataset, replacement=True, num_samples=int(1e10)
+        )
+        self.train_loader = DataLoader(dataset, sampler=train_sampler, **kwargs)
+        self.data_iter = iter(self.train_loader)
+
+    def next(self):
+        try:
+            batch = next(self.data_iter)
+        except StopIteration:  # this will technically only happen after 1e10 samples... (i.e. basically never)
+            self.data_iter = iter(self.train_loader)
+            batch = next(self.data_iter)
+        return batch
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="Makemore")
-  # Args: Framework (PyTorch, TF, JAX, MLX)
-  parser.add_argument("--framework", type=str, default="PyTorch")
-  # Args: Model (Bigram, MLP)
-  parser.add_argument("--model", type=str, default="Bigram")
-  # Backend (CPU, CUDA, Metal)
-  parser.add_argument("--backend", type=str, default="cpu")
-  # Args: File
-  parser.add_argument("--file", type=str, default="names.txt")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Makemore")
+    # Args: Framework (PyTorch, TF, JAX, MLX)
+    parser.add_argument("--framework", type=str, default="PyTorch")
+    # Args: Model (Bigram, MLP)
+    parser.add_argument("--model", type=str, default="Bigram")
+    # Backend (CPU, CUDA, Metal)
+    parser.add_argument("--backend", type=str, default="cpu")
+    # Args: File
+    parser.add_argument("--file", type=str, default="names.txt")
+    args = parser.parse_args()
